4d65c31118a1bd84403f19c4c9aa619132000fac
Øyvind Monsen <yvind.monsen@gmail.com>
2020-03-17 20:29:39 +0100

    added server url to cors allowed origins

--- src/main/java/com/oyvindmonsen/workout_tracker_api/CorsConfigurer.java
+++ src/main/java/com/oyvindmonsen/workout_tracker_api/CorsConfigurer.java
@@ -12,6 +12,7 @@ public class CorsConfigurer implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000");
+                .allowedOrigins("http://localhost:3000", "workout.oyvindmonsen.no");
+
     }
 }
